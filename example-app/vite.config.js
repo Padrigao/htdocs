87167d6cab096b71881e06f7cecf9952f11b7353
Padrigao <padrigaoclarkjames.com>
2024-02-12 11:07:32 +0800

    clark

--- example-app/vite.config.js
+++ example-app/vite.config.js
@@ -3,7 +3,7 @@ import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     plugins: [
-        laravel({
+        laravelcalrk({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
